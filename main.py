d48901a6d765eddee0d99e32bcab0d1960a242c5
lanceren <lancerenbj1308@163.com>
2020-03-26 13:01:30 +0800

    update

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import sys
-
+import os
 from GUI import gui_operator
 from License_Plate_Chars_Recognize import lpcr_operate
 from Image_Enhancement import imgEnhance_operate
-
 
 # gui_operator.Gui_Generator()
 # imgEnhance_operate.Ie_Operator()
