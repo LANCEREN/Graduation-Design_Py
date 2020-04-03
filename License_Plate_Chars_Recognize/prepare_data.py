04271acdbd7337f707879e987b8c55d95825f211
lanceren <lancerenbj1308@163.com>
2020-04-03 10:27:07 +0800

    update LPCR_PD

--- License_Plate_Chars_Recognize/prepare_data.py
+++ License_Plate_Chars_Recognize/prepare_data.py
@@ -14,57 +14,48 @@ class PrepareData():
                                                            'train_images', 'training-set')
         self.saverDirVal = globalVars.projectPath / Path('License_Plate_Chars_Recognize', 'data', 'dataset',
                                                          'train_images', 'validation-set')
-        self.trainTargetNumber = 34
 
-    def SampleToValid(self):
+    def SampleToValid(self, mode='digits'):
 
-        # 生成Digit评估测试的图片数据和标签
+        # 判断mode
+        def case1():  # 第一种情况执行的函数
+            self.inputDir = self.saverDirTrain
+            self.saveDir = self.saverDirVal
+            self.trainTargetNumber = 34
 
+        def case2():  # 第二种情况执行的函数
+            self.inputDir = self.saverDirTrain / Path('letters')
+            self.saveDir = self.saverDirVal / Path('letters')
+            self.trainTargetNumber = 24
+
+        def case3():  # 第三种情况执行的函数
+            self.inputDir = self.saverDirTrain / Path('chinese-characters')
+            self.saveDir = self.saverDirVal / Path('chinese-characters')
+            self.trainTargetNumber = 31
+
+        def default():  # 默认情况下执行的函数
+            print('No such case')
+
+        switch = {'digits': case1,  # 注意此处不要加括号
+                  'letters': case2,  # 注意此处不要加括号
+                  'provinces': case3,  # 注意此处不要加括号
+                  }
+
+        choice = mode  # 获取选择
+        switch.get(choice, default)()  # 执行对应的函数，如果没有就执行默认的函数
+
+        # 生成 mode 评估测试的图片数据和标签
         for i in range(0, self.trainTargetNumber):
             index = 0
-            inputDir = self.saverDirTrain / Path(f'{i}')
-            saveDir = self.saverDirVal / Path(f'{i}')
+            inputDir = self.inputDir / Path(f'{i}')
+            saveDir = self.saveDir / Path(f'{i}')
             if os.path.exists(saveDir.__str__()): shutil.rmtree(saveDir.__str__())
             os.mkdir(saveDir.__str__())
             for rt, dirs, files in os.walk(inputDir.__str__()):
                 for filename in files:
                     if index % 9 == 0:
                         fullFileName = inputDir / Path(filename)
-                        saveFile = saveDir / Path(f'{filename}.png')
-                        img = PIL.Image.open(fullFileName.__str__())
-                        img.save(saveFile.__str__())
-                    index += 1
-
-        # 生成Letters评估测试的图片数据和标签
-
-        for i in range(0, 24):
-            index = 0
-            inputDir = self.saverDirTrain / Path('letters', f'{i}')
-            saveDir = self.saverDirVal / Path('letters', f'{i}')
-            if os.path.exists(saveDir.__str__()): shutil.rmtree(saveDir.__str__())
-            os.mkdir(saveDir.__str__())
-            for rt, dirs, files in os.walk(inputDir.__str__()):
-                for filename in files:
-                    if index % 9 == 0:
-                        fullFileName = inputDir / Path(filename)
-                        saveFile = saveDir / Path(f'{filename}.png')
-                        img = PIL.Image.open(fullFileName.__str__())
-                        img.save(saveFile.__str__())
-                    index += 1
-
-        # 生成Provinces评估测试的图片数据和标签
-
-        for i in range(0, 31):
-            index = 0
-            inputDir = self.saverDirTrain / Path('chinese-characters', f'{i}')
-            saveDir = self.saverDirVal / Path('chinese-characters', f'{i}')
-            if os.path.exists(saveDir.__str__()): shutil.rmtree(saveDir.__str__())
-            os.mkdir(saveDir.__str__())
-            for rt, dirs, files in os.walk(inputDir.__str__()):
-                for filename in files:
-                    if index % 9 == 0:
-                        fullFileName = inputDir / Path(filename)
-                        saveFile = saveDir / Path(f'{filename}.png')
+                        saveFile = saveDir / Path(f'{filename}')
                         img = PIL.Image.open(fullFileName.__str__())
                         img.save(saveFile.__str__())
                     index += 1
